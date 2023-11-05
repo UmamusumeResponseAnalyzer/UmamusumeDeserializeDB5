a196db97077a17a3906526abb47fa6ae2244589d
离披 <lipi@shuise.dev>
2023-11-06 00:22:55 +0800

    对应URA的适配

--- Generator/SkillData.cs
+++ Generator/SkillData.cs
@@ -15,7 +15,7 @@ namespace UmamusumeDeserializeDB5.Generator
         Dictionary<long, string> IdToName = new();
         List<SkillData> list = new List<SkillData>();
 
-        public void Generate()
+        private void PrepareDB()
         {
             using var conn = new SQLiteConnection(new SQLiteConnectionStringBuilder { DataSource = UmamusumeDeserializeDB5.UmamusumeDatabaseFilePath }.ToString());
             conn.Open();
@@ -32,7 +32,9 @@ namespace UmamusumeDeserializeDB5.Generator
                         group_id = (long)reader["group_id"],
                         group_rate = (long)reader["group_rate"],
                         grade_value = (long)reader["grade_value"],
+                        precondition_1 = (string)reader["precondition_1"],
                         condition_1 = (string)reader["condition_1"],
+                        precondition_2 = (string)reader["precondition_2"],
                         condition_2 = (string)reader["condition_2"],
                         disp_order = (long)reader["disp_order"]
                     });
@@ -56,6 +58,10 @@ namespace UmamusumeDeserializeDB5.Generator
                     IdToName.Add((long)reader["index"], (string)reader["text"]);
                 }
             }
+        }
+        public void Generate()
+        {
+            PrepareDB();
             foreach (var i in SkillList)
             {
                 var skill = new SkillData
@@ -69,64 +75,96 @@ namespace UmamusumeDeserializeDB5.Generator
                     Cost = SkillNeedPointTable.ContainsKey(i.id) ? (int)SkillNeedPointTable[i.id] : 0,
                     DisplayOrder = (int)i.disp_order
                 };
-                if (i.condition_1.Contains("ground_type==1") || i.condition_2.Contains("ground_type==1"))
-                {
-                    skill.Ground = SkillData.GroundType.Turf;
-                }
-                else if (i.condition_1.Contains("ground_type==2") || i.condition_2.Contains("ground_type==2"))
-                {
-                    skill.Ground = SkillData.GroundType.Dirt;
-                }
-                else
-                {
-                    skill.Ground = SkillData.GroundType.None;
-                }
 
-                if (i.condition_1.Contains("running_style==1") || i.condition_2.Contains("running_style==1"))
+                var propers = new List<SkillData.SkillProper>();
+                propers.AddRange(i.precondition_1.Split("@").Select(x => ProcessCondition(x)));
+                propers.AddRange(i.condition_1.Split("@").Select(x => ProcessCondition(x)));
+                propers.AddRange(i.precondition_2.Split("@").Select(x => ProcessCondition(x)));
+                propers.AddRange(i.condition_2.Split("@").Select(x => ProcessCondition(x)));
+                if (propers.All(x =>
+                   x.Ground == SkillData.SkillProper.GroundType.None
+                && x.Style == SkillData.SkillProper.StyleType.None
+                && x.Distance == SkillData.SkillProper.DistanceType.None
+                ))
                 {
-                    skill.Style = SkillData.StyleType.Nige;
-                }
-                else if (i.condition_1.Contains("running_style==2") || i.condition_2.Contains("running_style==2"))
-                {
-                    skill.Style = SkillData.StyleType.Senko;
-                }
-                else if (i.condition_1.Contains("running_style==3") || i.condition_2.Contains("running_style==3"))
-                {
-                    skill.Style = SkillData.StyleType.Sashi;
-                }
-                else if (i.condition_1.Contains("running_style==4") || i.condition_2.Contains("running_style==4"))
-                {
-                    skill.Style = SkillData.StyleType.Oikomi;
+                    skill.Propers = Array.Empty<SkillData.SkillProper>();
                 }
                 else
                 {
-                    skill.Style = SkillData.StyleType.None;
-                }
-
-                if (i.condition_1.Contains("distance_type==1") || i.condition_2.Contains("distance_type==1"))
-                {
-                    skill.Distance = SkillData.DistanceType.Short;
-                }
-                else if (i.condition_1.Contains("distance_type==2") || i.condition_2.Contains("distance_type==2"))
-                {
-                    skill.Distance = SkillData.DistanceType.Mile;
-                }
-                else if (i.condition_1.Contains("distance_type==3") || i.condition_2.Contains("distance_type==3"))
-                {
-                    skill.Distance = SkillData.DistanceType.Middle;
-                }
-                else if (i.condition_1.Contains("distance_type==4") || i.condition_2.Contains("distance_type==4"))
-                {
-                    skill.Distance = SkillData.DistanceType.Long;
-                }
-                else
-                {
-                    skill.Distance = SkillData.DistanceType.None;
+                    skill.Propers = propers.Where(x =>
+                   x.Ground != SkillData.SkillProper.GroundType.None
+                || x.Style != SkillData.SkillProper.StyleType.None
+                || x.Distance != SkillData.SkillProper.DistanceType.None).ToArray();
                 }
                 list.Add(skill);
             }
 
             Save("skilldata", list);
+
+            SkillData.SkillProper ProcessCondition(string cond)
+            {
+                var skill = new SkillData.SkillProper();
+
+                // 场地适性
+                if (cond.Contains("ground_type==1"))
+                {
+                    skill.Ground = SkillData.SkillProper.GroundType.Turf;
+                }
+                else if (cond.Contains("ground_type==2"))
+                {
+                    skill.Ground = SkillData.SkillProper.GroundType.Dirt;
+                }
+                else
+                {
+                    skill.Ground = SkillData.SkillProper.GroundType.None;
+                }
+
+                // 跑法适性
+                if (cond.Contains("running_style==1"))
+                {
+                    skill.Style = SkillData.SkillProper.StyleType.Nige;
+                }
+                else if (cond.Contains("running_style==2"))
+                {
+                    skill.Style = SkillData.SkillProper.StyleType.Senko;
+                }
+                else if (cond.Contains("running_style==3"))
+                {
+                    skill.Style = SkillData.SkillProper.StyleType.Sashi;
+                }
+                else if (cond.Contains("running_style==4"))
+                {
+                    skill.Style = SkillData.SkillProper.StyleType.Oikomi;
+                }
+                else
+                {
+                    skill.Style = SkillData.SkillProper.StyleType.None;
+                }
+
+                // 距离适性
+                if (cond.Contains("distance_type==1"))
+                {
+                    skill.Distance = SkillData.SkillProper.DistanceType.Short;
+                }
+                else if (cond.Contains("distance_type==2"))
+                {
+                    skill.Distance = SkillData.SkillProper.DistanceType.Mile;
+                }
+                else if (cond.Contains("distance_type==3"))
+                {
+                    skill.Distance = SkillData.SkillProper.DistanceType.Middle;
+                }
+                else if (cond.Contains("distance_type==4"))
+                {
+                    skill.Distance = SkillData.SkillProper.DistanceType.Long;
+                }
+                else
+                {
+                    skill.Distance = SkillData.SkillProper.DistanceType.None;
+                }
+
+                return skill;
+            }
         }
     }
     public class SkillDataTable
@@ -136,7 +174,9 @@ namespace UmamusumeDeserializeDB5.Generator
         public long group_id { get; set; }
         public long group_rate { get; set; }
         public long grade_value { get; set; }
+        public string precondition_1 { get; set; }
         public string condition_1 { get; set; }
+        public string precondition_2 { get; set; }
         public string condition_2 { get; set; }
         public long disp_order { get; set; }
     }
@@ -155,31 +195,43 @@ namespace UmamusumeDeserializeDB5.Generator
         public int Grade;
         public int Cost;
         public int DisplayOrder;
-        public GroundType Ground;
-        public DistanceType Distance;
-        public StyleType Style;
+        public UpgradedSkillData[] Upgraded;
+        public SkillProper[] Propers;
 
-        public enum GroundType
+        public class SkillProper
         {
-            None,
-            Turf,
-            Dirt
+            public GroundType Ground;
+            public DistanceType Distance;
+            public StyleType Style;
+
+            public enum GroundType
+            {
+                None,
+                Turf,
+                Dirt
+            }
+            public enum DistanceType
+            {
+                None,
+                Short,
+                Mile,
+                Middle,
+                Long
+            }
+            public enum StyleType
+            {
+                None,
+                Nige,
+                Senko,
+                Sashi,
+                Oikomi
+            }
         }
-        public enum DistanceType
-        {
-            None,
-            Short,
-            Mile,
-            Middle,
-            Long
-        }
-        public enum StyleType
-        {
-            None,
-            Nige,
-            Senko,
-            Sashi,
-            Oikomi
-        }
+    }
+    public class UpgradedSkillData : SkillData
+    {
+        public int Owner;
+        public int Rank;
+        public long[] Conditions;
     }
 }
--- Generator/TalentSkillSet.cs
+++ Generator/TalentSkillSet.cs
@@ -10,10 +10,11 @@ namespace UmamusumeDeserializeDB5.Generator
 {
     internal class TalentSkillSet : GeneratorBase
     {
-        public void Generate()
+        List<AvailableSkillSetTable> AvailableSkillSetTableList = new();
+        List<SkillUpgradeConditionTable> SkillUpgradeConditionTables = new();
+        List<SkillUpgradeDescriptionTable> SkillUpgradeDescriptionTable = new();
+        void PrepareDB()
         {
-            var list = new List<AvailableSkillSetTable>();
-
             using var conn = new SQLiteConnection(new SQLiteConnectionStringBuilder { DataSource = UmamusumeDeserializeDB5.UmamusumeDatabaseFilePath }.ToString());
             conn.Open();
             using (var cmd = conn.CreateCommand())
@@ -22,7 +23,7 @@ namespace UmamusumeDeserializeDB5.Generator
                 var reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    list.Add(new AvailableSkillSetTable
+                    AvailableSkillSetTableList.Add(new AvailableSkillSetTable
                     {
                         available_skill_set_id = (long)reader["available_skill_set_id"],
                         skill_id = (long)reader["skill_id"],
@@ -30,20 +31,95 @@ namespace UmamusumeDeserializeDB5.Generator
                     });
                 }
             }
-
-            var result = list.GroupBy(x => x.available_skill_set_id).ToDictionary(x => x.Key, x => x.ToArray().Select(y => new
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = $"select * from skill_upgrade_condition";
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    SkillUpgradeConditionTables.Add(new SkillUpgradeConditionTable
+                    {
+                        id = (long)reader["id"],
+                        description_id = (long)reader["description_id"],
+                        num = (long)reader["num"],
+                        sub_num = (long)reader["sub_num"],
+                        timing_type = (long)reader["timing_type"],
+                        count_type = (long)reader["count_type"],
+                    });
+                }
+            }
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = $"select * from skill_upgrade_description";
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    SkillUpgradeDescriptionTable.Add(new SkillUpgradeDescriptionTable
+                    {
+                        id = (long)reader["id"],
+                        card_id = (long)reader["card_id"],
+                        rank = (long)reader["rank"],
+                        skill_id = (long)reader["skill_id"]
+                    });
+                }
+            }
+        }
+        public void Generate()
+        {
+            PrepareDB();
+            var conditions = SkillUpgradeConditionTables.GroupBy(x => x.description_id).ToDictionary(x => x.Key, x => x.Select(y => y.id).ToArray());
+            var result = AvailableSkillSetTableList.GroupBy(x => x.available_skill_set_id).ToDictionary(x => x.Key, x => x.ToArray().Select(y => new TalentSkill
             {
                 SkillId = y.skill_id,
                 Rank = y.need_rank
-            }));
-
+            }).ToList());
+            foreach (var i in result)
+            {
+                var upgraded = SkillUpgradeDescriptionTable.Where(x => x.card_id == i.Key);
+                foreach (var j in upgraded)
+                {
+                    switch (j.rank)
+                    {
+                        case 3:
+                            i.Value[i.Value.IndexOf(i.Value.First(x => x.Rank == 3))].UpgradeSkills.Add(j.skill_id, conditions[j.skill_id].ToArray());
+                            break;
+                        case 5:
+                            i.Value[i.Value.IndexOf(i.Value.First(x => x.Rank == 5))].UpgradeSkills.Add(j.skill_id, conditions[j.skill_id].ToArray());
+                            break;
+                        default:
+                            throw new Exception("出现了白技能的进化技能？");
+                    }
+                }
+            }
             Save("talentskillsets", result);
         }
+    }
+    public class TalentSkill
+    {
+        public long SkillId;
+        public long Rank;
+        public Dictionary<long, long[]> UpgradeSkills = new();
     }
     public class AvailableSkillSetTable
     {
         public long available_skill_set_id;
         public long skill_id;
         public long need_rank;
+    }
+    public class SkillUpgradeConditionTable
+    {
+        public long id;
+        public long description_id;
+        public long num;
+        public long sub_num;
+        public long timing_type;
+        public long count_type;
+    }
+    public class SkillUpgradeDescriptionTable
+    {
+        public long id;
+        public long card_id;
+        public long rank;
+        public long skill_id;
     }
 }
